a7778debd6d42eb77baf98d8294f6faf4af166f6
Bouthainan AlGarni <Bouthainhabdullah5@gmail.com>
2025-12-03 16:04:55 +0300

    adding missing methods to the interface

--- Controllers/IParentController.cs
+++ Controllers/IParentController.cs
@@ -19,6 +19,8 @@ namespace BadeePlatform.Controllers
         public Task<IActionResult> EditChildProfile(string childId, EditChildDTO dto);
         public IActionResult ViewChildDashboard(string childId);
         public Task<IActionResult> ParentHomePage();
+        public Task<IActionResult> ViewProfile();
+        public Task<IActionResult> EditProfile(ParentProfileViewModel model);
         public Task<IActionResult> GetSchoolsByCity(string city);
         public Task<IActionResult> GetGradesBySchool(string schoolId);
         public Task<IActionResult> GetClassesByGrade(string gradeId);
